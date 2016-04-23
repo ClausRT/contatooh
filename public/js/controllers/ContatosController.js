14a2a72fd8a9a750778fb41d67cf4e4a558fabf9
ClausRT <clausrt@outlook.com>
2016-04-23 00:35:10 -0300

    Bower and Angular.js implementation
    Capitulos 3 e 4 do livro.

---
+++ public/js/controllers/ContatosController.js
@@ -0,0 +1,29 @@
+//%scope é considerado o model no Angular.js
+//M e C do MVC no mesmo lugar
+
+angular.module('contatooh').controller('ContatosController', function ($scope) {
+    $scope.total = 0;
+    $scope.filtro = '';
+    
+    $scope.incrementa = function () {
+        $scope.total++;
+    };
+    
+    $scope.contatos = [
+        {
+            "_id": 1,
+            "nome": "Contato Angular 1",
+            "email": "cont1@empresa.com.br"
+        },
+        {
+            "_id": 2,
+            "nome": "Contato Angular 2",
+            "email": "cont2@empresa.com.br"
+        },
+        {
+            "_id": 3,
+            "nome": "Contato Angular 3",
+            "email": "cont3@empresa.com.br"
+        }
+    ];
+});
